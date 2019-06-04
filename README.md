2e8a711f859ebb1ba94dd060eb29e513a5ba7447
Matthew Byrd <matbyrd@microsoft.com>
2019-06-04 09:40:26 -0400

    Support for SCC connections

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # Project: Cloud Connect
 
-### Description:
+## Install
+The module is published to the PowerShell Gallery so it should be installed from there.
+https://www.powershellgallery.com/packages/CloudConnect/1.0.0
+
+Install-Module -Name CloudConnect
+
+## Description:
 PS Module for connecting to Shell / Graph API using ADAL to multiple Microsoft Cloud Services (O365)
 
-### Implemented Services:
-Exchange Online - Connect-EXO
+## Implemented Services:
+Exchange Online MFA                     - Connect-EXO
+Exchange Online Legacy                  - Connect-EXOLegacy
+Azure Graph                             - Connect-AzureGraph
+MSOnline PowerShell for Azure AD        - Connect-MSOLService (Imported from MSOL Module)
+Security and Compliance Center MFA      - Connect-SCC
+Security and Compliance Center Legacy   - Connect-SCCLegacy
 
-### Known Issues:
+## Known Issues:
 - New PSSession and Import must be done when refreshing the EXO token
+- New PSSession and Import must be done when refreshing the SCC token
 
-### License
+## License
 MIT License
---
+++ Services/Connect-SCC.ps1
@@ -0,0 +1,48 @@
+function Connect-SCC {
+
+    <#
+    .SYNOPSIS
+    Creates a connection to Exchange online using Oauth
+
+    .DESCRIPTION
+    Acquires a token from Azure AD
+    Uses that token to connect to Exchange Online Powershell
+
+    Token lifetime is 1 hour
+    The Caller must manage the session / token lifetime.
+    It will pull the token from the token cache for a silent refresh of the toke if it is there.
+
+    When the token expires if the session is not refreshed the Exchange shell will attempt implict remoting that will prompt / fail
+
+    .LINK
+    https://github.com/Canthv0/CloudConnect
+
+    .OUTPUTS
+    None.  Creates a PS session to SCC and imports it into the global scope
+
+    .EXAMPLE
+    Connect-SCC
+
+    Will prompt for credentials and import Security and Compliance Center PS Session into the global scope
+
+    #>
+
+    # Get the token from the service
+    $Token = Get-ServiceToken -service exo
+
+    # Check for an existing PS Session to EXO and remove it
+    Get-PSSession -name scc* | Remove-PSSession -Confirm:$false
+
+    # Build the auth information
+    $Authorization = "Bearer {0}" -f $Token.Result.AccessToken
+    $UserId = ($Token.Result.UserInfo.DisplayableId).tostring()
+    
+    # create the "basic" token to send to O365 EXO
+    $Password = ConvertTo-SecureString -AsPlainText $Authorization -Force
+    $Credtoken = New-Object System.Management.Automation.PSCredential -ArgumentList $UserId, $Password
+    
+    # Create and import the session
+    $Session = New-PSSession -Name SCC -ConfigurationName Microsoft.Exchange -ConnectionUri 'https://ps.compliance.protection.outlook.com/PowerShell-LiveId?BasicAuthToOAuthConversion=true' -Credential $Credtoken -Authentication Basic -AllowRedirection -ErrorAction Stop
+    Import-Module (Import-PSSession $Session -AllowClobber) -Global -WarningAction 'SilentlyContinue'
+
+}
